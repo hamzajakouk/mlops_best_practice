2270a50fb3c14743450cc49484ef3054663c1a69
Hamza Jakouk <hamzajakouk240@gmail.com>
2023-11-10 17:21:23 +0000

    requirement added

---
+++ hello.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def calculate():
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6])
+    c = a * b
+    return c
+
+if __name__ == "__main__":
+    print(calculate())
---
+++ test.py
@@ -0,0 +1,5 @@
+from hello import calculate
+
+
+def test_time():
+    assert 2 == calculate(1, 1)
